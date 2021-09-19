f73780e6f26a23cb52e23c8fe479424d80a93469
Guillaume <2261189+gdepouilly@users.noreply.github.com>
2021-09-19 14:20:09 +0200

    feat: Get Contentful articles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,25 @@
+import { createClient, Entry } from "contentful";
+import { GetStaticPropsResult } from "next";
+import { TypeArticle } from "../cf-types";
+
+
+export async function getStaticProps(): Promise<GetStaticPropsResult<Entry<TypeArticle>[]>> {
+
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID || "foo",
+    accessToken: process.env.CONTENTFUL_TOKEN || "foo"
+  });
+
+  const result = await client.getEntries( { "content-type": "article"} );
+
+  return {
+    props: {
+      articles: result.items
+    }
+  }
+}
+
+
 function Articles(): JSX.Element {
   return <div>Articles list goes here</div>;
 }
