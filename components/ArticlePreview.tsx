4188bae6c9d8317f418b974f29f792a486460ba3
Guillaume <guillaume.depouilly@gmail.com>
2021-10-07 20:52:30 +0200

    refactor: Create ArticlePreview component

--- components/ArticlePreview.tsx
+++ components/ArticlePreview.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Link from "next/link";
+import * as CFRichTextTypes from "@contentful/rich-text-types";
 import { TypeArticle } from "../cf-types";
 import { documentToPlainTextString } from "@contentful/rich-text-plain-text-renderer";
-import { pipe } from "ramda";
+import { pipe, slice } from "ramda";
 
-function getFirstCharacters(content: string) {
-  return content.substring(0, 20);
+const getTwentyFirstChararacters = (content: string) => slice(0, 20, content);
+
+function getArticlePreview(
+  articleContent: CFRichTextTypes.Block | CFRichTextTypes.Inline
+) {
+  return pipe(
+    documentToPlainTextString,
+    getTwentyFirstChararacters
+  )(articleContent);
 }
 
 interface ArticlePreviewProps {
@@ -18,13 +26,8 @@ export default function ArticlePreview(
   return (
     <div>
       {props.article.fields.titre}
-      <p>
-        {pipe(
-          documentToPlainTextString,
-          getFirstCharacters
-        )(props.article.fields.contenu)}
-      </p>
-      <Link href={"/articles/" + props.article.fields.slug}>
+      <p>{getArticlePreview(props.article.fields.contenu)}</p>
+      <Link href={`/articles/${props.article.fields.slug}`}>
         <a>Lire la suite</a>
       </Link>
       <br />
